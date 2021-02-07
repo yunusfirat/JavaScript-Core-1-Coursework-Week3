8f19e3752fe0a0b7ea007e6d5422887856b37e42
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2021-02-07 13:46:38 +0000

    ForEach

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,20 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+function transform(num) {
+  if(num % 3 == 0 && num % 5 == 0) {
+    return  "FizzBuzz"
+  }else if (num % 3 == 0){
+    return "Fizz"
+  }else if (num % 5 == 0) {
+    return "Buzz";
+  }else {
+    return num;
+  }
+  }
+
+var result = arr.map(transform).forEach(item => console.log(item))
+
 /* EXPECTED OUTPUT */
 
 /*

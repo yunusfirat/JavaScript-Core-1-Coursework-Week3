efdcc7cd3814f98fd117757565cee04e224e849b
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2021-02-07 13:09:11 +0000

    Array Filter

--- 1-exercises/D-array-filter/exercise.js
+++ 1-exercises/D-array-filter/exercise.js
@@ -8,8 +8,9 @@
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
 
+ // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter((arr) => Array.isArray(arr) && arr.length == 2); 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 

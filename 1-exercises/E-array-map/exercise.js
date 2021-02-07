a8fd21de8149ba56bce035fb3b0ac215afbea203
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2021-02-07 13:17:22 +0000

    Array Map

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,20 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+function multipliedby100(number) {
+  return number * 100;
+}
+
+var result = numbers.map(multipliedby100);
+
+console.log(result);
+
+var result2 = numbers.map((num) => num * 100);
+console.log(result2);
+
+var result3 = numbers.map(function multipliedby100(number) {
+  return number * 100;
+});
+
+console.log(result3);
